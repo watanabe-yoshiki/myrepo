de0c6677b98cc88291a05306e5c49c3f63821d83
watanabe-yoshiki <yoshiki@its.co.jp>
2026-02-05 16:57:54 +0900

    美容医療_デザイン修正

--- aesthetic-medicine-caution/test/js/navi.js
+++
@@ -1,8 +0,0 @@
-        document.addEventListener("DOMContentLoaded", function() {
-            const hamburger = document.querySelector(".hamburger");
-            const navUl = document.querySelector("nav ul");
-            
-            hamburger.addEventListener("click", function() {
-                navUl.classList.toggle("active");
-            });
-        });
